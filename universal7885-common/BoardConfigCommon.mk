7e0ea2450f102474aba01a80dee068ab4c15a4a5
Gabriel2392 <gabriel824m@gmail.com>
2021-12-11 06:11:47 -0300

    a10: add kernel arch

--- universal7885-common/BoardConfigCommon.mk
+++ universal7885-common/BoardConfigCommon.mk
@@ -50,6 +50,7 @@ TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_universal7885
 
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := androidboot.init_fatal_reboot_target=recovery
