77da0762a79fdefaae53f06f4628d6a7ee57eae7
aswin11234 <aswinsanker08@gmail.com>
2022-04-19 15:46:55 +0000

    a10 : Welcome Blaze

---
+++ a10/AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/blaze_a10.mk
+
+COMMON_LUNCH_CHOICES := \
+    blaze_a10-userdebug \
+    blaze_a10-user \
+    blaze_a10-eng
--- a10/blaze_a10.mk
+++ a10/blaze_a10.mk
@@ -9,9 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configuration
 $(call inherit-product, device/samsung/a10/device.mk)
 
+# Inherit some common Blaze stuff
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a10
-PRODUCT_NAME := full_a10
+PRODUCT_NAME := blaze_a10
 PRODUCT_MODEL := SM-A105F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+
+#Blaze Things
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 720
+BLAZE_MAINTAINER := dotA
