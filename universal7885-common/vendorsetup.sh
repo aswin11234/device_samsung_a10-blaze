3ee4e1604631cf4bb87392d8d6458d2522ae09d5
Aswin <73487313+aswin11234@users.noreply.github.com>
2022-05-09 15:04:20 +0800

    Vendorsetup.sh changes
    Replace eureka private kernel source with public kernel source and removed A10 setup.sh as A10 dt has adapted for blaze

--- universal7885-common/vendorsetup.sh
+++ universal7885-common/vendorsetup.sh
@@ -1,12 +1,5 @@
 FM_PATH="packages/apps/FMRadio"
-if [ -e ~/.git-credentials ]; then
-MODE="https://"
-SPERATOR="/"
-else
-MODE="git@"
-SPERATOR=":"
-fi
-git clone --depth=1 "$MODE"github.com"$SPERATOR"eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S-private.git -b R9.2_rom kernel/samsung/exynos7885
+git clone --depth=1 https://github.com/eurekadevelopment/Eureka-Kernel-Exynos7885-Q-R-S -b R9.2_rom kernel/samsung/exynos7885
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_libbt hardware/samsung_slsi/libbt
 git clone https://github.com/eurekadevelopment/android_hardware_samsung_slsi_scsc_wifibt_wifi_hal.git hardware/samsung_slsi/scsc_wifibt/wifi_hal
 git clone https://github.com/lineageos/android_hardware_samsung_slsi_scsc_wifibt_wpa_supplicant_lib hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
@@ -14,13 +7,6 @@ mv hardware/samsung/nfc .
 git clone https://github.com/eurekadevelopment/android_hardware_samsung hardware/samsung
 mv nfc hardware/samsung
 git clone --depth=1 https://github.com/eurekadevelopment/android_vendor_samsung_exynos7885.git -b master vendor/samsung
-if test -f device/samsung/universal7885-common/vendor_name; then
-rm device/samsung/universal7885-common/vendor_name
-fi
-python3 device/samsung/universal7885-common/vendor_detect/main.py
-echo "Generating A10 Makefiles"
-./device/samsung/a10/setup.sh
-
 # BT Call patch
 if ! grep -q ESCO_TRANSPORT_UNIT_SIZE system/bt/device/src/esco_parameters.cc; then
 echo "Applying BT call patch";
